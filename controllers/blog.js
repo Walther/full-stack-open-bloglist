14c7aac2cc62cd40f83941e61c1b07dd1ab01000
Walther <veeti.haapsamo@gmail.com>
2021-02-23 01:45:11 +0200

    Exercises 4.18 - 4.19

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,15 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("users");
@@ -8,10 +17,15 @@ blogRouter.get("/", async (request, response) => {
 });
 
 blogRouter.post("/", async (request, response, next) => {
-  // const user = await User.findById(body.userId);
-  // TODO: fix initial dirty impl as instructed by the instructions
-  const users = await User.find({});
-  const user = users[0];
+  const token = getTokenFrom(request);
+  if (!token) {
+    return response.status(401).json({ error: "token missing" });
+  }
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blogObject = {
     title: request.body.title,
